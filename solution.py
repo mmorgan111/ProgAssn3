bbd0dc2b05867822db6ebba6c05ef5b072c11d47
mmorgan111 <112628435+mmorgan111@users.noreply.github.com>
2022-11-12 01:52:13 -0500

    Add files via upload

--- solution.py
+++ solution.py
@@ -124,7 +124,7 @@ def ping(host, timeout=1):
 
     for i in range(0, 4):  # Four pings will be sent (loop runs for i=0, 1, 2, 3)
         delay, statistics = doOnePing(dest, timeout)  # what is stored into delay and statistics?
-        response = response.append({'bytes': str(round(struct.calcsize("d"), 2)), 'rtt': str(round(delay, 2)), 'ttl': str(round(statistics/1000000, 2))}, ignore_index=True)
+        response = response.append({'bytes': str(round(struct.calcsize("d"), 2)), 'rtt': delay, 'ttl': statistics / 1000000}, ignore_index=True)
         # store your bytes, rtt, and ttle here in your response pandas dataframe. An example is commented out below for vars
         print(response)
         time.sleep(1)  # wait one second
